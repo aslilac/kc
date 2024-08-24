d1e70f99eb29da386b30ac4662cbfec5d0f5b5dd
McKayla Washburn <mckayla@hey.com>
2024-08-23 23:30:57 -0600

    ignore node_modules always

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,11 @@
 use std::ffi::OsStr;
+use std::path::Component;
+use std::sync::LazyLock;
+
+static NODE_MODULES: LazyLock<Component> =
+	LazyLock::new(|| Component::Normal(OsStr::new("node_modules")));
+static PACKAGE_LOCK_JSON: LazyLock<&OsStr> = LazyLock::new(|| OsStr::new("package-lock.json"));
 
 pub fn default_ignore_rule(path: &ignore::DirEntry) -> bool {
-	path.file_name() != OsStr::new("package-lock.json")
+	path.path().components().all(|c| c != *NODE_MODULES) && path.file_name() != *PACKAGE_LOCK_JSON
 }
